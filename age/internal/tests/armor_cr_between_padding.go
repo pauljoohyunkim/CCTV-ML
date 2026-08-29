4448f2097b2daa812c91a26141f9f36c2096b9ca
Filippo Valsorda <hi@filippo.io>
2026-08-29 17:47:39 +0200

    age: add armor carriage return test vectors
    Reported by Joe Doyle of Trail of Bits.

---
+++ age/internal/tests/armor_cr_between_padding.go
@@ -0,0 +1,35 @@
+// Copyright 2026 The age Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build ignore
+// +build ignore
+
+package main
+
+import (
+	"strings"
+
+	"c2sp.org/CCTV/age/internal/testkit"
+)
+
+func main() {
+	f := testkit.NewTestFile()
+	f.VersionLine("v1")
+	f.X25519(testkit.TestX25519Identity)
+	f.HMAC()
+	f.Payload("age12")
+	file := f.Bytes()
+	f.Buf.Reset()
+	f.BeginArmor("AGE ENCRYPTED FILE")
+	f.Body(file)
+	f.Base64Padding()
+	line := f.UnreadLine()
+	if !strings.Contains(line, "==") {
+		panic("need two padding characters")
+	}
+	f.TextLine(strings.Replace(line, "==", "=\r=", 1))
+	f.EndArmor("AGE ENCRYPTED FILE")
+	f.ExpectArmorFailure()
+	f.Generate()
+}
